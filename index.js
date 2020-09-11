88d08867e63f0d944c8daebcf92be76b5e71b99b
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-09-10 22:37:57 -0300

    base work

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+
+const app=express();
+
+
+//public
+app.use(express.static('public'));
+
+// app.get('/',(req,res)=>{
+// res.json({
+//     ok:true
+// })
+// })
+
+
+const port =process.env.PORT
+
+app.listen(port, ()=>{
+    console.log(`Servidor corriendo en puerto ${port}`);
+})
